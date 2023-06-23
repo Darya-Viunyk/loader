2625ab1895251070d1c1609f21b537a91475cba9
Darya-Viunyk <dar.viunyk@gmail.com>
2023-06-24 00:42:56 +0300

    Added task 2

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 import React from "react-dom";
-import { Box, BoxWave, Wave } from "./LoaderViolet.styled";
-// import { Box, BoxWave, Wave } from "./LoaderGreen.styled";
+
+import { Box, BoxWave, Wave } from "./Loader.styled";
 export const Loader = () => {
   const waveElements = Array.from({ length: 20 }, (_, index) => (
     <Wave key={index} />
