76af9f661269b582917f2f14edd722eb72fe2828
Darya-Viunyk <dar.viunyk@gmail.com>
2023-06-24 01:12:02 +0300

    Added task 4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Loader } from "components/Loader";
-import "./index.css";
+import "./components/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
